f92b9161b04938be25f88604c78aa3044bc9cf3d
RafaelMatos <rafael-matos@outlook.com>
2023-05-30 23:49:50 -0300

    created upload file route

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,20 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // todas URLs de front poderao acessar nosso back
@@ -17,10 +27,12 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333')
